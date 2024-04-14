75d947749d487ead55a54fcffa4c1a7009b52029
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-14 15:29:04 +0200

    adding custom folders in storybook

--- src/components/badge/Badge.stories.ts
+++ src/components/badge/Badge.stories.ts
@@ -5,7 +5,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Badge from "./Badge";
 
 const meta = {
-  title: "Example/Badge",
+  title: "Components/Badge",
   component: Badge,
   parameters: { layout: "centered" },
   tags: ["autodocs"],
