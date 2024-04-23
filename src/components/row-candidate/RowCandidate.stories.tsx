8ecfc848cf8611b805abae09cc763ef9c344070c
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-23 13:11:59 +0200

    refatored

--- src/components/row-candidate/RowCandidate.stories.tsx
+++ src/components/row-candidate/RowCandidate.stories.tsx
@@ -40,6 +40,26 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     id: 1,
+    candidate_name: "Eduardo Alvarez Nowak",
+    candidate_job_title: "Tech Lead",
+    candidate_image_url: ImageURLs.candidate_eduardo,
+    company_name: "Novare Potential",
+    company_duration_in_months: 34,
+    company_image_url: ImageURLs.company_novare,
+    notes:
+      "Highly qualified, likes to help people and stays up to date with the latest frontend development tools.",
+    relevance: 5,
+    contact_status: 1,
+    contact_date: "2024-04-02 21:00:30.610279",
+    projectId: 1,
+    date_created: "2024-01-31 21:00:30.610279",
+    linked_in_url: "https://www.linkedin.com/in/eduardo-alvarez-nowak/",
+  },
+};
+
+export const LongTexts: Story = {
+  args: {
+    id: 1,
     candidate_name: "Eduardo Xavier Alvarez Nowak",
     candidate_job_title: "Tech Lead specializing in frontend",
     candidate_image_url: ImageURLs.candidate_eduardo,
